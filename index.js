ce6bbd9799a3857726dee771844847331a5eff3e
c0d3runn3r <joseph.sullivan@active911.com>
2017-06-28 16:53:20 -0700

    Bugfix

--- index.js
+++ index.js
@@ -276,7 +276,7 @@ class SockhopClient extends EventEmitter{
 
 							// Call the callback instead of bubbling the event
 							this._send_callbacks[o.callback_id](o.data, {type:o.type});
-							this._send_callbacks.delete(o.callback_id);
+							delete this._send_callbacks[o.callback_id];
 
 						// Remote end is requesting callback
 						} else if (o.id){
@@ -676,7 +676,7 @@ class SockhopServer extends EventEmitter {
 
 								// Call the callback instead of bubbling the event
 								_self._send_callbacks[o.callback_id](o.data, {type:o.type});
-								_self._send_callbacks.delete(o.callback_id);
+								delete _self._send_callbacks[o.callback_id];
 
 							// Remote end is requesting callback
 							} else if (o.id){

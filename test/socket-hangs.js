ae64a3f0f61125510d39a4cb99082ecb86a86880
Jonathan Van Schenck <jvschenck@novadynamics.com>
2025-10-07 13:24:45 -0700

    lint

--- test/socket-hangs.js
+++ test/socket-hangs.js
@@ -6,19 +6,19 @@ var c,s,sock,sock_connect;
 
 describe("Client Socket Hangups",function () {
 
-    
+
     c=new Sockhop.client({port: 50000});
     sock = new net.Socket(); // create a mock socket
-      
+
     this.slow(1000);
-   
+
     //Restore the connect function on each completed
     afterEach(function () {
         sock_connect.restore();
     });
 
     it("throws timeout error on connection hang", (done)=>{
-        
+
         // create a custom mock object that returns the mock socket when createConnection is called
         sock_connect = sinon.stub(net, 'createConnection').callsFake(() => {
             return sock;
@@ -27,11 +27,11 @@ describe("Client Socket Hangups",function () {
         c._connect_timeout = 200;
 
         c.connect()
-        .catch((e)=>{
-            assert(e.message.match("Timeout exceeded on connect()"));
-            done();
-        })
+            .catch((e)=>{
+                assert(e.message.match("Timeout exceeded on connect()"));
+                done();
+            });
     });
-   
+
 
 });

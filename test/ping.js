87432875aac7d539ba6c586f2b4109d57564f0d8
Jonathan Van Schenck <jvschenck@novadynamics.com>
2025-10-08 10:29:30 -0700

    Ensure pings still function even in compatibility mode

--- test/ping.js
+++ test/ping.js
@@ -1,155 +1,192 @@
 var Sockhop=require("../index.js");
+const expect=require("chai").expect;
 
 var c,s,s2;
 
-describe("Ping", function(){
+let BASE_PORT=52000;
+const HANDSHAKE_TIMEOUT=200;
 
-    this.slow(3000);
-    s=new Sockhop.server({port: 50004});
-    c=new Sockhop.client({port: 50004});
-    s2=new Sockhop.server({port: 50006});
 
-    it("Client pings server successfully for 500ms", function(done){
+for ( const { server_compatibility_mode, client_compatibility_mode } of [
+    { server_compatibility_mode: false, client_compatibility_mode: false },
+    { server_compatibility_mode: true, client_compatibility_mode: false },
+    { server_compatibility_mode: false, client_compatibility_mode: true },
+    { server_compatibility_mode: true, client_compatibility_mode: true },
+] ) {
 
-        s.listen()
-            .then(()=>c.connect())
-            .then(()=>{
+    describe(`Ping (server_compatibility_mode=${server_compatibility_mode}, client_compatibility_mode=${client_compatibility_mode})`, function(){
 
+        it("Client pings server successfully for 1000ms", async function(){
+            let port=BASE_PORT++;
+            const s=new Sockhop.server({port: port, compatibility_mode: server_compatibility_mode, handshake_timeout: HANDSHAKE_TIMEOUT});
+            const c=new Sockhop.client({port: port, compatibility_mode: client_compatibility_mode, handshake_timeout: HANDSHAKE_TIMEOUT});
+
+            try {
+                await s.listen();
+                await c.connect();
+
+                // Setup disconnect listener
+                let disconnected = false;
                 s.once("disconnect",()=>{
-
-                    throw new Error("Server has disconnected unexpectedly");
+                    disconnected = true;
                 });
 
-                c.ping(100);        // Ping at 100ms intervals
+                c.ping(100);   // Ping at 100ms intervals
 
-                setTimeout(()=>{    // Let it ping for 500ms, then done
+                await new Promise(r => setTimeout(r, 1000)); // Let it ping for 1000ms (plenty of time for multiple pings)
 
-                    s.removeAllListeners("disconnect");
+                expect(disconnected).to.be.false; // Ensure server did not disconnect
+
+            } finally {
+                await s.close();
+                await c.disconnect();
+                await new Promise(r=>setTimeout(r,100));
+            }
+
+        });
+
+        it("Simultaneous ping client<-->server for 1000ms", async function(){
+
+            let port=BASE_PORT++;
+            const s=new Sockhop.server({port: port, compatibility_mode: server_compatibility_mode, handshake_timeout: HANDSHAKE_TIMEOUT});
+            const c=new Sockhop.client({port: port, compatibility_mode: client_compatibility_mode, handshake_timeout: HANDSHAKE_TIMEOUT});
+
+            try {
+                await s.listen();
+                await c.connect();
+
+                // Setup disconnect listener
+                let client_disconnected = false;
+                s.once("disconnect",()=>{
+                    client_disconnected = true;
+                });
+
+                let server_disconnected = false;
+                c.once("disconnect",()=>{
+                    server_disconnected = true;
+                });
+
+                c.ping(100);   // Ping at 100ms intervals
+                s.ping(100);   // Ping at 100ms intervals
+
+                await new Promise(r => setTimeout(r, 1000)); // Let it ping for 1000ms (plenty of time for multiple pings)
+
+                expect(client_disconnected,"Client disconnected from server").to.be.false; // Ensure server did not disconnect
+                expect(server_disconnected,"Server disconnected from client").to.be.false; // Ensure client did not disconnect
+
+            } finally {
+                await s.close();
+                await c.disconnect();
+                await new Promise(r=>setTimeout(r,100));
+            }
+        });
+
+        it("Server disconnects paused client from ping-out", async function(){
+
+            let port=BASE_PORT++;
+            const s=new Sockhop.server({port: port, compatibility_mode: server_compatibility_mode, handshake_timeout: HANDSHAKE_TIMEOUT});
+            const c=new Sockhop.client({port: port, compatibility_mode: client_compatibility_mode, handshake_timeout: HANDSHAKE_TIMEOUT});
+
+            try {
+                await s.listen();
+                await c.connect();
+
+
+                let disconnected = false;
+                s.once("disconnect",() => {
+                    disconnected = true;
+                });
+
+                s.ping(100);
+                c.socket.pause();    // Should cause the server to kill us
+
+                await new Promise(r => setTimeout(r, 1000)); // Wait to be disconnected
+
+                expect(disconnected).to.be.true; // Ensure server disconnected
+
+            } finally {
+                await s.close();
+                await c.disconnect();
+                await new Promise(r=>setTimeout(r,100));
+            }
+        });
+
+        it("Client disconnects paused client from ping-out", async function(){
+            this.timeout(10000);
+
+            let port=BASE_PORT++;
+            const s=new Sockhop.server({port: port, compatibility_mode: server_compatibility_mode, handshake_timeout: HANDSHAKE_TIMEOUT});
+            const c=new Sockhop.client({port: port, compatibility_mode: client_compatibility_mode, handshake_timeout: HANDSHAKE_TIMEOUT});
+
+            try {
+                await s.listen();
+                await c.connect();
+
+
+                let disconnected = false;
+                c.once("disconnect",() => {
+                    disconnected = true;
+                });
+
+                c.ping(100);
+                s.sockets.map((s)=>s.pause()); // Should cause the client to kill us
+
+                await new Promise(r => setTimeout(r, 1000)); // Wait to be disconnected
+
+                expect(disconnected).to.be.true; // Ensure server disconnected
+
+            } finally {
+                await s.close();
+                await c.disconnect();
+                await new Promise(r=>setTimeout(r,100));
+            }
+        });
+
+        it("Client can reconnect elsewhere after ping-ing-out from server", async function(){
+            this.timeout(10000);
+
+            let port=BASE_PORT++;
+            let port2=BASE_PORT++;
+            const s=new Sockhop.server({port: port, compatibility_mode: server_compatibility_mode, handshake_timeout: HANDSHAKE_TIMEOUT});
+            const s2=new Sockhop.server({port: port2, compatibility_mode: server_compatibility_mode, handshake_timeout: HANDSHAKE_TIMEOUT});
+            const c=new Sockhop.client({port: port, compatibility_mode: client_compatibility_mode, handshake_timeout: HANDSHAKE_TIMEOUT});
+
+            try {
+                await s.listen();
+                await s2.listen();
+                await c.connect();
+
+
+                let disconnected = false;
+                c.once("disconnect",() => {
+                    disconnected = true;
+
                     c.ping(0);
-                    done();
+                    c.port=port2;
+                    c._end_socket();
+                    c.connect();
+                });
 
-                }, 500);
+                let reconnected = false;
+                c.once("connect",() => {
+                    reconnected = true;
+                });
 
-            });
-    });
+                c.ping(100);
+                s.sockets.map((s)=>s.pause()); // Should cause the client to kill us
 
+                await new Promise(r => setTimeout(r, 1000)); // Wait to be disconnected
 
-    it("Simultaneous ping client<-->server for 500ms", function(done){
+                expect(disconnected).to.be.true; // Ensure server disconnected
+                expect(reconnected).to.be.true; // Ensure we reconnected
 
-        s.once("disconnect",()=>{
-
-            throw new Error("Server has disconnected unexpectedly");
-        })
-            .ping(100);        // Ping at 100ms intervals
-
-        setTimeout(()=>{    // Let it ping for 500ms, then disconnect client and done
-
-            s.removeAllListeners("disconnect");
-            s.ping(0);
-            c.disconnect();
-            done();
-
-        }, 500);
-    });
-
-    it("Server disconnects paused client (should be slow)", function(done){
-
-        // c.on("error",(e)=>{
-
-        //     console.log("CLIENT THREW!!!");
-        // });
-
-        // We are done once disconnect fires
-        s.once("disconnect",()=>{
-
-            s.ping(0);
-            done();
-        });
-
-        // Connect and then pause the socket
-        c.connect().then(()=>{
-
-            s.ping(200);
-            c.socket.pause();    // Should cause the server to kill us
+            } finally {
+                await s.close();
+                await c.disconnect();
+                await new Promise(r=>setTimeout(r,100));
+            }
         });
 
     });
 
-    it("Client disconnects paused server (should be slow)", function(done){
-
-        c=new Sockhop.client({port: 50004});
-        c.once("disconnect",()=>{
-
-            s.disconnect();
-            c.ping(0);
-            done();
-        });
-        c.connect().then(()=>{
-
-            c.ping(200);
-            s.sockets.map((s)=>s.pause());
-        });
-
-
-    });
-
-
-    it("Client reconnects on disconnect (should be slow)", function(done){
-
-        this.slow(10000);
-        this.timeout(10000);
-
-        s.close();
-        s=new Sockhop.server({port: 50005});
-
-        c=new Sockhop.client({port: 50005});
-        // Set up disconnect event to reconnect
-        c.once("disconnect",()=>{
-
-            // We now set up an event handler so we are done when we reconnect (to a different server, actually, so we can do it fast)
-            c.port=50006;
-            c.once("connect",()=>{
-
-                c.ping(0);
-                done();
-            });
-
-
-            c._end_socket();
-            c.connect();
-        });
-
-        s.listen()
-            .then(()=>{
-
-                return s2.listen();
-            })
-            .then(()=>{
-
-                return c.connect();
-            })
-            .then(()=>{
-
-                // Ping from client while pausing server (causes disconnect)
-                c.ping(200, true);
-                s.sockets.map((s)=>s.pause());
-
-            });
-
-
-    });
-
-
-    after(("close servers"),()=>{
-
-        s.close();
-        s2.close();
-    });
-});
-
-
-
-
-
-
-
+}
